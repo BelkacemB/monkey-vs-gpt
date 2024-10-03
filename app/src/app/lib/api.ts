1502ef62b39c8bba8472e7859c47b5f98601d808
Belkacem Berchiche <b_berchiche@esi.dz>
2024-10-03 12:17:02 +0200

    Cache API calls

--- app/src/app/lib/api.ts
+++ app/src/app/lib/api.ts
@@ -1,31 +1,33 @@
+import { cache } from 'react';
+
 const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
 
-export async function getPortfolios(): Promise<PortfolioData> {
+export const getPortfolios = cache(async (): Promise<PortfolioData> => {
     const res = await fetch(`${baseUrl}/api/portfolios`, {
-        cache: 'no-store',
+        next: { revalidate: 7200 }, // Cache for 2 hours (7200 seconds)
     });
     if (!res.ok) {
         throw new Error('Failed to fetch portfolios');
     }
     return res.json();
-}
+});
 
-export async function getTrades() {
+export const getTrades = cache(async () => {
     const res = await fetch(`${baseUrl}/api/trades`, {
-        cache: 'no-store',
+        next: { revalidate: 7200 }, // Cache for 2 hours (7200 seconds)
     });
     if (!res.ok) {
         throw new Error('Failed to fetch trades');
     }
     return res.json();
-}
+});
 
-export async function getValuations() {
+export const getValuations = cache(async () => {
     const res = await fetch(`${baseUrl}/api/valuations`, {
-        cache: 'no-store',
+        next: { revalidate: 7200 }, // Cache for 2 hours (7200 seconds)
     });
     if (!res.ok) {
         throw new Error('Failed to fetch valuations');
     }
     return res.json();
-}
+});
